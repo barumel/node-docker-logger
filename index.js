9d2e01d07bcc520dc5ab1bd2142b46e278d7c679
Manuel Kipfer <manuel.kipfer@swisscom.com>
2017-07-12 23:05:44 +0200

    - Fix various bugs - Adjust date format

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const Logger = require('./lib/Logger')
+const Logger = require('./lib/Logger');
+const fs = require('fs-extra');
+
+const socket = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
+const stats = fs.statSync(socket);
 
 const logger = new Logger({
   socket: socket,
-  baseDir: '/Users/onkel_peter/Playground/docker/share-sock/data'
+  baseDir: process.env.BASE_DIR || '/var/log/'
 });
 logger.start();
